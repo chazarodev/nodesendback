e06c8ef0820ac199b55b71cc57cdc47fa9947e0b
Alberto Cházaro <shazarerom@gmail.com>
2021-08-15 23:27:06 -0500

    Aplicación NodeSend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const conectarDB = require('./config/db')
+
+//Crear el servidor 
+const app = express();
+
+//Conectar a la base de datos
+conectarDB()
+
+console.log('Comenzando Node Send')
+
+//Puerto de la app
+const port = process.env.PORT || 4000;
+
+//Habilitar leer los valores de un body
+app.use(express.json())
+
+//Rutas de la app
+app.use('/api/usuarios', require('./routes/usuarios'))
+
+//Arrancar la app
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor está funcionando en el puerto ${port}`)
+})
